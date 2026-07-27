377f3ea0d24a22d9592dc1cf4c75792e82ab73f0
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2026-07-27 18:25:28 +0600

    Sidebar component layout for improved structure and readability

--- src/components/pages/AiChatbox/ChatBoxSidebar.tsx
+++ src/components/pages/AiChatbox/ChatBoxSidebar.tsx
@@ -12,7 +12,7 @@ type Props = {
 export default function Sidebar({ selectedUser, onSelectUser, users, isLoading }: Props) {
   return (
     <div className="flex min-h-0 flex-col rounded-xl border border-[#DFE1E7] bg-white ">
-      <div className="min-h-0 flex-1 overflow-y-auto custom-scroll p-3 ">
+      <div className="flex min-h-0 flex-1 flex-col p-3">
         <div className="relative mb-2">
           <Search
             size={16}
@@ -24,13 +24,15 @@ export default function Sidebar({ selectedUser, onSelectUser, users, isLoading }
             className=" w-full rounded-2xl bg-[#f8fafb] border border-[#DFE1E7] px-3.75 py-3.5  text-sm outline-none"
           />
         </div>
-        {isLoading ? (
-          <div className="p-4 text-center text-sm text-gray-500">Loading users...</div>
-        ) : users.length ? (
-          <UserList selectedUser={selectedUser} onSelectUser={onSelectUser} users={users} />
-        ) : (
-          <div className="p-4 text-center text-sm text-gray-500">No users available.</div>
-        )}
+        <div className="min-h-0 flex-1 overflow-y-auto custom-scroll">
+          {isLoading ? (
+            <div className="p-4 text-center text-sm text-gray-500">Loading users...</div>
+          ) : users.length ? (
+            <UserList selectedUser={selectedUser} onSelectUser={onSelectUser} users={users} />
+          ) : (
+            <div className="p-4 text-center text-sm text-gray-500">No users available.</div>
+          )}
+        </div>
       </div>
     </div>
   );
